5906f81fb8db2590782cc742bf5784cfc0f46d99
pharooque <32064752+pharooque@users.noreply.github.com>
2025-02-04 15:02:18 +0100

    implement GET request function with response handling

--- main.cpp
+++ main.cpp
@@ -11,3 +11,20 @@ std::string_view getURL()
 
     return url;
 }
+
+void getRequest(const std::string_view& url)
+{
+    std::cout << "Making GET Request to: " << url << std::endl;
+
+    cpr::Response response = cpr::Get(cpr::Url{url});
+    if (response.status_code == 200)
+    {
+        std::cout << "GET Request successful." << std::endl;
+        std::cout << "Response: " << response.text << std::endl;
+    }
+    else
+    {
+        std::cout << "GET Request failed." << std::endl;
+    }
+
+}
